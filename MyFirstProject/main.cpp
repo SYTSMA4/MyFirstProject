fc9fdb660c334103aa85bdb09c5dd2aa267a48d3
SYTSMA4 <sytsma4@tritons.iowacentral.edu>
2022-08-25 10:47:39 -0500

    Initial Commit

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyFirstProject
+//
+//  Created by Sam Sytsma on 8/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
